feba3bb9d585423698941b7c2769b3d2e99bc378
Ukasiu <git@ukasiu.pl>
2014-05-19 15:08:00 +0200

    Weird test failing

--- app/models/changeset_comment.rb
+++ app/models/changeset_comment.rb
@@ -2,10 +2,11 @@ class ChangesetComment < ActiveRecord::Base
   belongs_to :changeset, :touch => true
   belongs_to :author, :class_name => "User"
 
-  validates_presence_of :id, :on => :update
+  validates_presence_of :id, :on => :update # is it necessary?
   validates_uniqueness_of :id
   validates_presence_of :changeset_id
   validates_associated :changeset
+  validates_presence_of :author_id
   validates_associated :author
 
   # Return the comment text
--- test/models/changeset_comment_test.rb
+++ test/models/changeset_comment_test.rb
@@ -6,4 +6,25 @@ class ChangesetCommentTest < ActiveSupport::TestCase
   def test_changeset_comment_count
     assert_equal 2, ChangesetComment.count
   end
+
+  # validations
+  def test_does_not_accept_invalid_author
+    comment = changeset_comments(:t1)
+    
+    comment.author = nil
+    assert !comment.valid?
+
+    comment.author_id = 999111
+    assert !comment.valid? # it's valid and it shoudln't?
+  end
+
+  def test_does_not_accept_invalid_changeset
+    comment = changeset_comments(:t1)
+
+    comment.changeset = nil
+    assert !comment.valid?
+
+    comment.changeset_id = 999111
+    assert !comment.valid? # it's valid and it shoudln't?
+  end
 end

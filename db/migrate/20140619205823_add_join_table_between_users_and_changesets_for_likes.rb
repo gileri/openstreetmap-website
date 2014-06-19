2e90a2a0ba80fe563d226d3055016b5773ae71b3
Łukasz Gurdek <git@ukasiu.pl>
2014-06-19 23:15:00 +0200

    Added migration

---
+++ db/migrate/20140619205823_add_join_table_between_users_and_changesets_for_likes.rb
@@ -0,0 +1,12 @@
+require 'migrate'
+
+class AddJoinTableBetweenUsersAndChangesets < ActiveRecord::Migration
+  def change
+    create_table :changesets_likers, id: false do |t|
+      t.column :liker_id, :bigint, null: false
+      t.column :changeset_id, :bigint, null: false
+    end
+    add_foreign_key :changesets_likers, [:liker_id], :users, [:id]
+    add_foreign_key :changesets_likers, [:changeset_id], :changesets, [:id]
+  end
+end

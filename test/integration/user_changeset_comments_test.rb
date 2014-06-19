d1bc626f0276bb52a9d9d7ad392efc8502c95539
Łukasz Gurdek <git@ukasiu.pl>
2014-06-19 22:50:14 +0200

    Little changeset comments integration tests refactor

--- test/integration/user_changeset_comments_test.rb
+++ test/integration/user_changeset_comments_test.rb
@@ -3,14 +3,30 @@ require 'test_helper'
 class UserChangesetCommentsTest < ActionDispatch::IntegrationTest
   fixtures :users, :changesets, :changeset_comments
 
-  # Test the creation of a changeset comment
-  def test_showing_create_changeset_comment
+  # Test 'log in to comment' message for nonlogged in user
+  def test_log_in_message
+    get "/changeset/#{changesets(:normal_user_closed_change).id}"
+    assert_response :success
+    
+    assert_select "div#content" do
+      assert_select "div#sidebar" do
+        assert_select "div#sidebar_content" do
+          assert_select "div.browse-section" do
+            assert_select "div.notice.hide_if_logged_in"
+          end
+        end
+      end
+    end
+  end
+
+  # Test if the form is shown
+  def test_displaying_form
     get_via_redirect '/login'
     # We should now be at the login page
     assert_response :success
     assert_template 'user/login'
     # We can now login
-    post  '/login', {'username' => "test@openstreetmap.org", 'password' => "test", :referer => '/diary/new'}
+    post  '/login', {'username' => "test@openstreetmap.org", 'password' => "test"}
     assert_response :redirect
 
     get "/changeset/#{changesets(:normal_user_closed_change).id}"
@@ -18,11 +34,6 @@ class UserChangesetCommentsTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_template 'browse/changeset'
 
-    # We will make sure that the form exists here, full 
-    # assert testing of the full form should be done in the
-    # functional tests rather than this integration test
-    # There are some things that are specific to the integration
-    # that need to be tested, which can't be tested in the functional tests
     assert_select "div#content" do
       assert_select "div#sidebar" do
         assert_select "div#sidebar_content" do

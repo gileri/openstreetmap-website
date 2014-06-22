5dee64c3663dc1388420e9dbf26a0ece85f10e09
Łukasz Gurdek <git@ukasiu.pl>
2014-06-22 20:36:36 +0200

    Added missing class to hide/unhide buttons

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ OpenStreetMap::Application.routes.draw do
   match 'api/0.6/changeset/:id/download' => 'changeset#download', :via => :get, :as => :changeset_download, :id => /\d+/
   match 'api/0.6/changeset/:id/expand_bbox' => 'changeset#expand_bbox', :via => :post, :id => /\d+/
   get 'api/0.6/changeset/:id' => 'changeset#read', :as => :changeset_read, :id => /\d+/, :defaults => { :format => :xml }
-  match 'api/0.6/changeset/:id/subscribe' => 'changeset#subscribe', :via => :post, :as => :changeset_subscribe, :id => /\d+/ # TODO ASK if it should go not via api
+  match 'api/0.6/changeset/:id/subscribe' => 'changeset#subscribe', :via => :post, :as => :changeset_subscribe, :id => /\d+/
   match 'api/0.6/changeset/:id/unsubscribe' => 'changeset#unsubscribe', :via => :post, :as => :changeset_unsubscribe, :id => /\d+/
   match 'api/0.6/changeset/:id' => 'changeset#update', :via => :put, :id => /\d+/
   match 'api/0.6/changeset/:id/close' => 'changeset#close', :via => :put, :id => /\d+/
